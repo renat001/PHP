58aaebf2509e5c406803c0056bdc363baa8b32a4
renat001 <Otanerjacul@icloud.com>
2025-09-23 17:42:25 +0200

    lesson 12

---
+++ lesson 12/header.php
@@ -0,0 +1,10 @@
+<!DocTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Digital school</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+
